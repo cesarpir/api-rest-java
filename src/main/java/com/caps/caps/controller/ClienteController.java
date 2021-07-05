fb4fdec0bdde7c727e60531ebe015d5c1620525a
Cesar Dos Santos <xandepir@gmail.com>
2021-07-05 15:02:38 -0300

    status created post

--- src/main/java/com/caps/caps/controller/ClienteController.java
+++ src/main/java/com/caps/caps/controller/ClienteController.java
@@ -4,6 +4,7 @@ import com.caps.caps.mapper.ClienteMapper;
 import com.caps.caps.schema.ClienteSchema;
 import com.caps.caps.service.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
@@ -35,6 +37,7 @@ public class ClienteController {
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public ClienteSchema insertCliente(@Valid @RequestBody ClienteSchema request) {
         return schemaMapper.INSTANCE.convert(service.insertCliente(request));
     }
